fc2c3ca3a2ea4a112a3f8d49e29268b3343d1ab9
David Edler <david.edler@canonical.com>
2025-06-04 23:51:09 +0200

    iterate on charm
    Signed-off-by: David Edler <david.edler@canonical.com>

--- src/charm.py
+++ src/charm.py
@@ -5,7 +5,6 @@
 import logging
 import ops
 
-# Log messages can be retrieved using juju debug-log
 logger = logging.getLogger(__name__)
 
 
@@ -17,30 +16,16 @@ class LanguageToolCharm(ops.CharmBase):
         self.pebble_service_name = "language-tool"
         framework.observe(self.on.language_tool_pebble_ready, self._on_language_tool_pebble_ready)
 
+
     def _on_language_tool_pebble_ready(self, event: ops.PebbleReadyEvent) -> None:
-        """Define and start a workload using the Pebble API.
-
-        Change this example to suit your needs. You'll need to specify the right entrypoint and
-        environment configuration for your specific workload.
-
-        Learn more about interacting with Pebble at https://juju.is/docs/sdk/pebble
-        Learn more about Pebble layers at
-            https://canonical-pebble.readthedocs-hosted.com/en/latest/reference/layers/
-        """
-        # Get a reference the container attribute on the PebbleReadyEvent
         container = event.workload
-        # Add initial Pebble config layer using the Pebble API
         container.add_layer("language_tool", self._pebble_layer, combine=True)
-        # Make Pebble reevaluate its plan, ensuring any services are started if enabled.
         container.replan()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
         self.unit.status = ops.ActiveStatus()
 
 
     @property
     def _pebble_layer(self) -> ops.pebble.Layer:
-        # Start the LanguageTool HTTP server
         command = " ".join(
             [
                 'java',
