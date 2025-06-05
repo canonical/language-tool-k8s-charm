9c16b4ec20655956ef2434eab9d91f430a40a24b
David Edler <david.edler@canonical.com>
2025-06-05 10:58:21 +0200

    simplify charm
    Signed-off-by: David Edler <david.edler@canonical.com>

--- src/charm.py
+++ src/charm.py
@@ -9,8 +9,6 @@ logger = logging.getLogger(__name__)
 
 
 class LanguageToolCharm(ops.CharmBase):
-
-
     def __init__(self, framework: ops.Framework) -> None:
         super().__init__(framework)
         self.pebble_service_name = "language-tool"
@@ -19,39 +17,8 @@ class LanguageToolCharm(ops.CharmBase):
 
     def _on_language_tool_pebble_ready(self, event: ops.PebbleReadyEvent) -> None:
         container = event.workload
-        container.add_layer("language_tool", self._pebble_layer, combine=True)
         container.replan()
         self.unit.status = ops.ActiveStatus()
-
-
-    @property
-    def _pebble_layer(self) -> ops.pebble.Layer:
-        command = " ".join(
-            [
-                'java',
-                '-cp',
-                '/opt/LanguageTool-6.6/languagetool-server.jar',
-                'org.languagetool.server.HTTPServer',
-                '--port',
-                '8081',
-                '--config',
-                '/opt/server.properties',
-                '--allow-origin'
-            ]
-        )
-        pebble_layer: ops.pebble.LayerDict = {
-            "summary": "Language tool http server",
-            "description": "pebble config layer for Language tool http server",
-            "services": {
-                self.pebble_service_name: {
-                    "override": "replace",
-                    "summary": "language tool http server",
-                    "command": command,
-                    "startup": "enabled",
-                }
-            },
-        }
-        return ops.pebble.Layer(pebble_layer)
 
 
 if __name__ == "__main__":  # pragma: nocover
